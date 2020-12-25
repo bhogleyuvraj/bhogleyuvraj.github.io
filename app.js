c845e9f8e669a43b138b44273314351dd3255737
bhogleyuvraj <bhogleyuvraj@outlook.com>
2020-12-25 23:01:09 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+//Movement Animation to happen
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+//Items
+const title = document.querySelector(".title");
+const sneaker = document.querySelector(".sneaker img");
+const purchase = document.querySelector(".purchase");
+const description = document.querySelector(".info h3");
+const sizes = document.querySelector(".sizes");
+const circle = document.querySelector(".circle");
+
+const layer1 = document.querySelector(".layer1");
+const layer2 = document.querySelector(".layer2");
+
+//Moving Animation Event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+//Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+  //Popout
+  title.style.transform = "translateZ(150px)";
+  sneaker.style.transform = "translateZ(100px)";
+  description.style.transform = "translateZ(125px)";
+  sizes.style.transform = "translateZ(100px)";
+  purchase.style.transform = "translateZ(75px)";
+  circle.style.transform = "scale(1,1)";
+  layer1.style.transform = "translateZ(100px) translateY(8px)";
+  layer2.style.transform = "translateZ(50px) translateY(4px)";
+});
+//Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  //Popback
+  title.style.transform = "translateZ(0px)";
+  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  description.style.transform = "translateZ(0px)";
+  sizes.style.transform = "translateZ(0px)";
+  purchase.style.transform = "translateZ(0px)";
+  layer1.style.transform = "translateZ(0px) translateY(0px)";
+  layer2.style.transform = "translateZ(0px) translateY(0px)";
+});
