79743952a92ae89a233ebf295241afe254223b5a
bhogleyuvraj <bhogleyuvraj@outlook.com>
2020-12-25 23:06:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const layer2 = document.querySelector(".layer2");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 50;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 50;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
 //Animate In
